3d24eab697a4c77b16906ee2cca01aa7178b1fc6
Usman Naveed <usman.naveed@2iqresearch.com>
2025-07-30 18:22:33 +0500

    creating react app and added button tab

---
+++ spring-ai-demo-react/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import React, {useState} from "react";
+function App() {
+
+ const [activeTab, setActiveTab] = useState('image-generator');
+
+ const handleTabChange = (tab) => {
+    alert(tab)
+    setActiveTab(tab)
+ }
+
+  return (
+    <div className="App">
+        <button onClick={() => handleTabChange('image-generator')}>Image Generator</button>
+        <button onClick={() => handleTabChange('chat')}>Chat </button>
+        <button onClick={() => handleTabChange('recipe-generator')}>Recipe Generator</button>
+
+        <div>
+            {activeTab === 'image-generator' && <h2>Image Generator</h2>}
+            {activeTab === 'chat' && <h2>Chat</h2>}
+            {activeTab === 'recipe-generator' && <h2>Recipe Generator</h2>}
+        </div>
+    </div>
+  );
+}
+
+export default App;
